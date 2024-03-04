f8f3c600d334be064011a7bba4e8b10b9e66e7e0
nhat0120230 <nhatnguyen221020032@gmail.com>
2024-03-04 13:28:20 +0700

    complete

---
+++ src/main/java/org/example/customermanagementjpa/repo/ICustomerRepository.java
@@ -0,0 +1,6 @@
+package org.example.customermanagementjpa.repo;
+
+import org.example.customermanagementjpa.model.Customer;
+
+public interface ICustomerRepository extends IGenerateRepository<Customer> {
+}
---
+++ src/main/java/org/example/customermanagementjpa/repo/IGenerateRepository.java
@@ -0,0 +1,10 @@
+package org.example.customermanagementjpa.repo;
+
+import java.util.List;
+
+public interface IGenerateRepository<T> {
+    List<T> findAll();
+    T findById(Long id);
+    void save(T t);
+    void remove(Long id);
+}
